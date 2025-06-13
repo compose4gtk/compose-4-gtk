8a8313607b3e7f5192a007731b1aac349e03353d
Leopold Luley <git@leopoldluley.de>
2025-06-13 12:47:12 +0200

    Add Adw.ButtonContent

---
+++ lib/src/main/kotlin/io/github/compose4gtk/adw/components/ButtonContent.kt
@@ -0,0 +1,28 @@
+package io.github.compose4gtk.adw.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ComposeNode
+import io.github.compose4gtk.GtkApplier
+import io.github.compose4gtk.GtkComposeWidget
+import io.github.compose4gtk.LeafComposeNode
+import io.github.compose4gtk.modifier.Modifier
+import org.gnome.adw.ButtonContent
+
+@Composable
+fun ButtonContent(
+    modifier: Modifier = Modifier,
+    label: String = "",
+    iconName: String = "",
+    useUnderline: Boolean = false,
+    canShrink: Boolean = false
+) {
+    ComposeNode<GtkComposeWidget<ButtonContent>, GtkApplier>({
+        LeafComposeNode(ButtonContent.builder().build())
+    }) {
+        set(modifier) { applyModifier(it) }
+        set(label) { this.widget.label = it }
+        set(iconName) { this.widget.iconName = it }
+        set(useUnderline) { this.widget.useUnderline = it }
+        set(canShrink) { this.widget.canShrink = it }
+    }
+}
