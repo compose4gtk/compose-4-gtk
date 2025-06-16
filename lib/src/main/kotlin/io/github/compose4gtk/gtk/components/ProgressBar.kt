32cd7373a89ea06595b369d40f272f2ee7b74612
Thomas Lavoie <lavoiethomas17@gmail.com>
2025-06-15 22:26:09 -0400

    Add state

--- lib/src/main/kotlin/io/github/compose4gtk/gtk/components/ProgressBar.kt
+++ lib/src/main/kotlin/io/github/compose4gtk/gtk/components/ProgressBar.kt
@@ -2,46 +2,70 @@ package io.github.compose4gtk.gtk.components
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.ComposeNode
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import io.github.compose4gtk.GtkApplier
 import io.github.compose4gtk.GtkComposeWidget
 import io.github.compose4gtk.LeafComposeNode
 import io.github.compose4gtk.modifier.Modifier
-import kotlinx.coroutines.delay
 import org.gnome.gtk.ProgressBar
 import org.gnome.pango.EllipsizeMode
 
+sealed interface ProgressBarState {
+    val progressBar: ProgressBar?
+    var fraction: Double
+    fun pulse()
+    fun reset()
+}
+
+private class ProgressBarStateImpl : ProgressBarState {
+    override var progressBar: ProgressBar? = null
+        set(value) {
+            check(field == null) { "ProgressBarState can be associated to a single ProgressBar" }
+            requireNotNull(value)
+            field = value
+        }
+
+    override var fraction: Double = 0.0
+
+    override fun pulse() {
+        progressBar?.pulse()
+    }
+
+    override fun reset() {
+        progressBar?.fraction = fraction
+    }
+}
+
+@Composable
+fun rememberProgressBarState(fraction: Double): ProgressBarState {
+    val state = remember { ProgressBarStateImpl() }
+    state.fraction = fraction
+    return state
+}
+
 @Composable
 fun ProgressBar(
+    state: ProgressBarState,
     modifier: Modifier = Modifier,
-    pulse: Boolean = false,
-    pulseInterval: Long = 250L,
     ellipsize: EllipsizeMode = EllipsizeMode.NONE,
-    fraction: Double = 0.0,
     inverted: Boolean = false,
     pulseStep: Double = 0.1,
     showText: Boolean = false,
     text: String? = null,
 ) {
-    val progressBar = remember { ProgressBar.builder().build() }
-
-    LaunchedEffect(pulse, pulseInterval) {
-        while (pulse) {
-            progressBar.pulse()
-            delay(pulseInterval)
-        }
-        progressBar.fraction = fraction
+    val stateImpl: ProgressBarStateImpl = when (state) {
+        is ProgressBarStateImpl -> state
     }
-
     ComposeNode<GtkComposeWidget<ProgressBar>, GtkApplier>(
         factory = {
-            LeafComposeNode(progressBar)
+            val gObject = ProgressBar()
+            stateImpl.progressBar = gObject
+            LeafComposeNode(gObject)
         },
         update = {
+            set(state.fraction) { this.widget.fraction = it }
             set(modifier) { applyModifier(it) }
             set(ellipsize) { this.widget.ellipsize = it }
-            set(fraction) { this.widget.fraction = it }
             set(inverted) { this.widget.inverted = it }
             set(pulseStep) { this.widget.pulseStep = it }
             set(showText) { this.widget.showText = it }
--- lib/src/test/kotlin/ProgressBar.kt
+++ lib/src/test/kotlin/ProgressBar.kt
@@ -1,3 +1,4 @@
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -9,8 +10,10 @@ import io.github.compose4gtk.adw.components.StatusPage
 import io.github.compose4gtk.gtk.components.ProgressBar
 import io.github.compose4gtk.gtk.components.ToggleButton
 import io.github.compose4gtk.gtk.components.VerticalBox
+import io.github.compose4gtk.gtk.components.rememberProgressBarState
 import io.github.compose4gtk.modifier.Modifier
 import io.github.compose4gtk.modifier.cssClasses
+import kotlinx.coroutines.delay
 
 fun main(args: Array<String>) {
     adwApplication("my.example.hello-app", args) {
@@ -19,14 +22,24 @@ fun main(args: Array<String>) {
                 HeaderBar(modifier = Modifier.cssClasses("flat"))
 
                 var pulsate by remember { mutableStateOf(false) }
+                val progressBarState = rememberProgressBarState(0.25)
+                val pulsatingProgressBarState = rememberProgressBarState(0.0)
+
+                LaunchedEffect(pulsate) {
+                    while (pulsate) {
+                        pulsatingProgressBarState.pulse()
+                        delay(250L)
+                    }
+                    pulsatingProgressBarState.reset()
+                }
 
                 StatusPage(
                     title = "Progress Bar",
                     description = "Display the progress of a long running operation",
                 ) {
                     VerticalBox(spacing = 8) {
-                        ProgressBar(fraction = 0.25, showText = true)
-                        ProgressBar(pulse = pulsate, pulseStep = 0.25)
+                        ProgressBar(state = progressBarState, showText = true)
+                        ProgressBar(state = pulsatingProgressBarState, pulseStep = 0.25)
                         ToggleButton(label = "Pulsate", active = pulsate, onToggle = { pulsate = !pulsate })
                     }
                 }
