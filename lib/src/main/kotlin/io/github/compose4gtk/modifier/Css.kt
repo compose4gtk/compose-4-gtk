fb024387bb6bd9c3e51bda45d216c4970cc382f2
Thomas Lavoie <80085756+Calvicii@users.noreply.github.com>
2025-07-06 13:36:04 +0000

    Add Adw.ButtonRow (#75)
    * Add Adw.ButtonRow
    
    * Fix Detekt issues
    
    * Undo only the added classes

--- lib/src/main/kotlin/io/github/compose4gtk/modifier/Css.kt
+++ lib/src/main/kotlin/io/github/compose4gtk/modifier/Css.kt
@@ -1,11 +1,27 @@
 package io.github.compose4gtk.modifier
 
 fun Modifier.cssClasses(vararg classes: String) = combine(
-    apply = { it.cssClasses = classes },
-    undo = { it.cssClasses = emptyArray() },
+    apply = {
+        for (cssClass in classes) {
+            it.addCssClass(cssClass)
+        }
+    },
+    undo = {
+        for (cssClass in classes) {
+            it.removeCssClass(cssClass)
+        }
+    },
 )
 
 fun Modifier.cssClasses(classes: List<String>) = combine(
-    apply = { it.cssClasses = classes.toTypedArray() },
-    undo = { it.cssClasses = emptyArray() },
+    apply = {
+        for (cssClass in classes) {
+            it.addCssClass(cssClass)
+        }
+    },
+    undo = {
+        for (cssClass in classes) {
+            it.removeCssClass(cssClass)
+        }
+    },
 )
