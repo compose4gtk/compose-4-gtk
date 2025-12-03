44a56189554bd1421035705071f3dff643cd2f0c
calvicii <lavoiethomas17@gmail.com>
2025-12-03 03:22:09 -0500

    Fix Detekt errors

--- lib/src/test/kotlin/Spinner.kt
+++ lib/src/test/kotlin/Spinner.kt
@@ -1,10 +1,8 @@
 import io.github.compose4gtk.adw.adwApplication
 import io.github.compose4gtk.adw.components.ApplicationWindow
 import io.github.compose4gtk.adw.components.HeaderBar
-import io.github.compose4gtk.gtk.components.HorizontalBox
-import io.github.compose4gtk.adw.components.Spinner as AdwSpinner
 import io.github.compose4gtk.adw.components.StatusPage
-import io.github.compose4gtk.gtk.components.Spinner as GtkSpinner
+import io.github.compose4gtk.gtk.components.HorizontalBox
 import io.github.compose4gtk.gtk.components.VerticalBox
 import io.github.compose4gtk.modifier.Modifier
 import io.github.compose4gtk.modifier.alignment
@@ -12,6 +10,8 @@ import io.github.compose4gtk.modifier.cssClasses
 import io.github.compose4gtk.modifier.expand
 import io.github.compose4gtk.modifier.sizeRequest
 import org.gnome.gtk.Align
+import io.github.compose4gtk.adw.components.Spinner as AdwSpinner
+import io.github.compose4gtk.gtk.components.Spinner as GtkSpinner
 
 fun main(args: Array<String>) {
     adwApplication("my.example.hello-app", args) {
