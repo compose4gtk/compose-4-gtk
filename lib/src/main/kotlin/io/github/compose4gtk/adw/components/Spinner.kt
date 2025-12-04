fdf8b4cd129592214968b9b9d6d44f104aac5876
Thomas Lavoie <80085756+Calvicii@users.noreply.github.com>
2025-12-04 07:03:10 -0500

    Add Adw Spinner with test (#87)
    * Add adw spinner with test
    
    * Fix Detekt errors

--- lib/src/main/kotlin/io/github/compose4gtk/adw/components/Spinner.kt
+++ lib/src/main/kotlin/io/github/compose4gtk/adw/components/Spinner.kt
@@ -1,4 +1,4 @@
-package io.github.compose4gtk.gtk.components
+package io.github.compose4gtk.adw.components
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.ComposeNode
@@ -6,23 +6,20 @@ import io.github.compose4gtk.GtkApplier
 import io.github.compose4gtk.GtkComposeWidget
 import io.github.compose4gtk.LeafComposeNode
 import io.github.compose4gtk.modifier.Modifier
-import org.gnome.gtk.Spinner as GtkSpinner
+import org.gnome.adw.Spinner as AdwSpinner
 
 /**
- * Creates a [org.gnome.gtk.Spinner] that displays an icon-size spinning animation.
+ * Creates a [org.gnome.adw.Spinner] that displays an icon-size spinning animation.
  *
  * @param modifier Compose [Modifier] for layout and styling.
- * @param spinning Whether the spinner should be spinning.
  */
 @Composable
 fun Spinner(
     modifier: Modifier = Modifier,
-    spinning: Boolean = false,
 ) {
-    ComposeNode<GtkComposeWidget<GtkSpinner>, GtkApplier>({
-        LeafComposeNode(GtkSpinner.builder().build())
+    ComposeNode<GtkComposeWidget<AdwSpinner>, GtkApplier>({
+        LeafComposeNode(AdwSpinner.builder().build())
     }) {
         set(modifier) { applyModifier(it) }
-        set(spinning) { this.widget.spinning = it }
     }
 }
