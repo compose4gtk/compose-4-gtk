f03fce16d9265b679e0910a4210ccd5965da97f2
brisson <gbrisson98@gmail.com>
2026-01-30 20:07:02 -0300

    Add selection signals for GtkListBox

--- lib/src/main/kotlin/io/github/compose4gtk/gtk/components/ListBox.kt
+++ lib/src/main/kotlin/io/github/compose4gtk/gtk/components/ListBox.kt
@@ -12,6 +12,7 @@ import io.github.compose4gtk.VirtualComposeNodeContainer
 import io.github.compose4gtk.modifier.Modifier
 import org.gnome.gtk.Adjustment
 import org.gnome.gtk.ListBox
+import org.gnome.gtk.ListBoxRow
 import org.gnome.gtk.SelectionMode
 import org.gnome.gtk.Widget
 
@@ -38,7 +39,13 @@ private class GtkListBoxComposeNode(gObject: ListBox) : GtkContainerComposeNode<
  *  - header func
  *  - placeholder
  *  - sort func
- *  - selections
+ *
+ *  @param onSelectRow This signal is emitted when a new row is selected, or `null` when the selection is cleared.
+ *  @param onSelectedRowsChanges This signal is emitted when the set of selected rows changes.
+ *  @param onSelectAll This signal is a keybinding signal which gets emitted to select all children of the box, if
+ *  the selection mode permits it. The default bindings for this signal is `Ctrl-a`.
+ *  @param onUnselectAll This signal is a keybinding signal which gets emitted to unselect all children of the box,
+ *  if the selection mode permits it. The default bindings for this signal is `Ctrl-Shift-a`.
  */
 @Composable
 fun ListBox(
@@ -47,6 +54,10 @@ fun ListBox(
     adjustment: Adjustment? = null,
     selectionMode: SelectionMode = SelectionMode.SINGLE,
     showSeparators: Boolean = false,
+    onSelectRow: (row: ListBoxRow?) -> Unit = {},
+    onSelectedRowsChanges: () -> Unit = {},
+    onSelectAll: () -> Unit = {},
+    onUnselectAll: () -> Unit = {},
     placeholder: @Composable () -> Unit = {},
     content: @Composable () -> Unit,
 ) {
@@ -60,6 +71,10 @@ fun ListBox(
             set(adjustment) { this.widget.adjustment = adjustment }
             set(selectionMode) { this.widget.selectionMode = selectionMode }
             set(showSeparators) { this.widget.showSeparators = showSeparators }
+            set(onSelectRow) { this.widget.onRowSelected { onSelectRow(it) } }
+            set(onSelectedRowsChanges) { this.widget.onSelectedRowsChanged { onSelectedRowsChanges() } }
+            set(onSelectAll) { this.widget.onSelectAll { onSelectAll() } }
+            set(onUnselectAll) { this.widget.onUnselectAll { onUnselectAll() } }
         },
         content = {
             ListBoxPlaceHolder(placeholder)
